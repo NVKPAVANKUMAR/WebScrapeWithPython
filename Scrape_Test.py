96b7b37de8852d3041e2f4e275d9d75ebf4f802f
NVKPAVANKUMAR <npavankumar144@gmail.com>
2018-09-13 18:27:39 +0530

    changes done on 13/09

--- Scrape_Test.py
+++ Scrape_Test.py
@@ -1,13 +1,24 @@
 import csv
 import urllib2
 from datetime import datetime
-from bs4 import BeautifulSoup
 
-quote_page = 'https://www.x-rates.com/calculator/?from=USD&to=INR&amount=1'
-page = urllib2.urlopen(quote_page)
-soup = BeautifulSoup(page, 'html.parser')
-price_box = soup.find('span', attrs={"class": 'ccOutputRslt'})
-price = price_box.text.strip("INR")
-with open("CurrencyValue.csv", 'ab') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(["USD2INR", price, datetime.now()])
+import HtmlTestRunner
+from bs4 import BeautifulSoup
+import unittest
+
+
+class CurrencyValueScrape(unittest.TestCase):
+
+    def test_scrape(self):
+        quote_page = 'https://www.x-rates.com/calculator/?from=USD&to=INR&amount=1'
+        page = urllib2.urlopen(quote_page)
+        soup = BeautifulSoup(page, 'html.parser')
+        price_box = soup.find('span', attrs={"class": 'ccOutputRslt'})
+        price = price_box.text.strip("INR")
+        with open("CurrencyValue.csv", 'ab') as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(["USD2INR", price, datetime.now()])
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output="example_dir"))
