baac6a7b9e20987d43f9d467b7af13540745c48f
NVKPAVANKUMAR <npavankumar144@gmail.com>
2018-11-08 14:43:28 +0530

    windows automation added

--- RequestsLibrary.py
+++ RequestsLibrary.py
@@ -1,4 +1,6 @@
 import unittest
+from pprint import pprint
+
 import requests
 
 from unittestExample import config_parser
@@ -7,11 +9,11 @@ from unittestExample import config_parser
 class TestRequests(unittest.TestCase):
     def test_get(self):
         try:
-         usn = config_parser(self, "Credentials", "username")
-         pas = config_parser(self, "Credentials", "password")
-         r = requests.get("https://reqres.in/api/login",
-                         auth=(usn, pas))
-         assert r.status_code == 200
+            usn = config_parser(self, "Credentials", "username")
+            pas = config_parser(self, "Credentials", "password")
+            r = requests.get("https://reqres.in/api/login",
+                             auth=(usn, pas))
+            assert r.status_code == 200
         except AssertionError as error:
             print("GET API Failed.", error)
         print(r.status_code)
@@ -19,3 +21,11 @@ class TestRequests(unittest.TestCase):
         print(r.encoding)
         print(r.text)
         print(r.json())
+
+    def test_JsonServer(self):
+        r = requests.options("http://localhost:3000/posts")
+        pprint(r.status_code)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
