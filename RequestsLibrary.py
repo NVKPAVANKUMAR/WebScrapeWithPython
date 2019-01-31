6e23f4babb111933cee7f63fdb3eeb0c9b1b889f
NVKPAVANKUMAR <npavankumar144@gmail.com>
2019-01-31 15:21:30 +0530

    modified request object

--- RequestsLibrary.py
+++ RequestsLibrary.py
@@ -2,28 +2,31 @@ import unittest
 from pprint import pprint
 import requests
 from unittestExample import config_parser
+import pytest
 
 
 class TestRequests(unittest.TestCase):
+    r = None
+
     def test_get(self):
         try:
-            usn = config_parser(self, "Credentials", "username")
-            pas = config_parser(self, "Credentials", "password")
-            r = requests.get("https://reqres.in/api/login",
-                             auth=(usn, pas))
-            assert r.status_code == 200
+            usn = config_parser("Credentials", "username")
+            pas = config_parser("Credentials", "password")
+            self.r = requests.get("https://reqres.in/api/login",
+                                  auth=(usn, pas))
+            assert self.r.status_code == 201
         except AssertionError as error:
             print("GET API Failed.", error)
-        print(r.status_code)
-        print(r.headers['content-type'])
-        print(r.encoding)
-        print(r.text)
-        print(r.json())
+            print(self.r.status_code)
+            print(self.r.headers['content-type'])
+            print(self.r.encoding)
+            print(self.r.text)
+            print(self.r.content)
 
+    @pytest.mark.skip
     def test_JsonServer(self):
-        r = requests.options("http://localhost:3000/posts")
-        pprint(r.status_code)
+        self.r = requests.options("http://localhost:3000/posts")
+        pprint(self.r.status_code)
 
-
-if __name__ == "__main__":
-    unittest.main(verbosity=2)
+    if __name__ == "__main__":
+        unittest.main(verbosity=2)
