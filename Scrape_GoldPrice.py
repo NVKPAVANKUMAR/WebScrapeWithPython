4b2a8e9f0334875e836bec498ab30a157fe91d4a
NVKPAVANKUMAR <npavankumar144@gmail.com>
2019-02-04 14:00:49 +0530

    goldprice scrapetest added

---
+++ Scrape_GoldPrice.py
@@ -0,0 +1,27 @@
+import unittest
+import urllib3
+from bs4 import BeautifulSoup
+import HtmlTestRunner
+
+
+class TestGoldPriceValue(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.base_url = 'https://www.goodreturns.in/gold-rates/vijayawada.html'
+        urllib3.disable_warnings()
+
+    def test_gold_price(self):
+        http = urllib3.PoolManager()
+        response = http.request("POST", self.base_url)
+        print(response.data)
+        soup = BeautifulSoup(response.data, "html.parser")
+        date_box = soup.find('div', attrs={'id': "current-date"})
+        price_box = soup.find('div', attrs={'id': "el"})
+        print(date_box, price_box)
+        price = price_box.text.strip('')
+        print(price)
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='test-reports'))
